77461489dd1942800b4f4f860404f4c92c725ac0
fabio bento da silva <fabiovenon01@hotmail.com>
2025-03-17 23:35:01 +0000

    Resultado

---
+++ n2_au3.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main (){
+    /*
+    Incremento (++)
+    Pré-Incremento ++a
+    Pós-Incremento a++
+    Decremento (--)
+    Pré-Decremento --a
+    Pós-Decremento a--
+    */
+
+    int numero1 = 1;
+
+    printf("Antes incremento: %d\n", numero1);
+    numero1++;
+    printf("Apos incremento: %d\n", numero1);
+
+
+}
