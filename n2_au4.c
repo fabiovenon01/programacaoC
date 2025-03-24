5778e531e2f37d30cbc96e0c2b014d5bfb2d200c
fabio bento da silva <fabiovenon01@hotmail.com>
2025-03-24 21:29:02 +0000

    Aula 3 - Numeros Inteiros na soma, subtração e etc

--- n2_au4.c
+++ n2_au4.c
@@ -1,1 +1,26 @@
+#include <stdio.h>
 
+int main (){
+    int a = 10;
+    int b = 3;
+    int soma = a + b;
+    int diferenca = a - b;
+    int produto = a * b;
+    int quociente = a / b; //Note que a divisão de inteiros resulta em um numero inteiro
+
+    printf("Soma: %d\n", soma);
+    printf("Diferença: %d\n", diferenca);
+    printf("Produto: %d\n", produto);
+    printf("Quociente: %d\n", quociente);
+
+
+
+
+
+
+
+
+
+return 0;
+
+}
