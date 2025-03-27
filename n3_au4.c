bce40664f7ddf3eb035d5921776f26ec291a183d
fabio bento da silva <fabiovenon01@hotmail.com>
2025-03-27 01:31:05 +0000

    Aula 5 - long long int

---
+++ n3_au4.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main () {
+    int numeroNormal = 2147483647; // valor maximo de int
+    long long int numeroGrande = 2147483647;
+
+    printf("Numero Regular (int): %d\n", numeroNormal);
+    printf("Numero Grande (long int): %lld\n", numeroGrande);
+
+
+return 0;
+
+}
